fc3a58cc994cafd0f03ff48dacec8508503230ff
Dallin Parker <dallin.r.parker@gmail.com>
2017-08-23 01:04:14 -0400

    basic structure with running node backend

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = module.exports = express()
+app.set('port', process.env.PORT || 5000)
+
+// MIDDLEWARE FOR EVERYTHING TO PASS THROUGH ================
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.listen(app.get('port'), () => {
+	console.log('listening on: ', app.get('port'))
+})
